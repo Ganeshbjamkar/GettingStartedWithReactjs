c3d5c6e1a5c542f70077505fad19a9d166ea2fee
Ganeshbjamkar <you@example.com>
2023-11-11 11:05:46 +0530

    Functional components with parameters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
+// App.js
+import React from "react";
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 300.65,
+      date: new Date(2023, 8, 20),
+      locationofexpenditure: "Online Insurance Company",
+    },
+    {
+      id: "e2",
+      title: "Restaurant",
+      amount: 700,
+      date: new Date(2023, 8, 20),
+      locationofexpenditure: "Hotel",
+    },
+    {
+      id: "e3",
+      title: "Taxi",
+      amount: 400,
+      date: new Date(2023, 8, 20),
+      locationofexpenditure: "Travel",
+    },
+    {
+      id: "e4",
+      title: "Grocery",
+      amount: 200,
+      date: new Date(2023, 8, 20),
+      locationofexpenditure: "Shop",
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      {expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          locationofexpenditure={expense.locationofexpenditure}
+        />
+      ))}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,18 @@
-function ExpenseItem() {
+// ExpenseItem.js
+import React from "react";
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
   return (
-    <div>
-      <h2>Expense Items</h2>
-      <p>Food Rs 10</p>
-      <p>Petrol Rs 100</p>
-      <p>Movies Rs 200</p>
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__locationofexpenditure">
+          {props.locationofexpenditure}
+        </div>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
     </div>
   );
 }
