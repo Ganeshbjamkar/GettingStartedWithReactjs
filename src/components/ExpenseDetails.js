17f8cd61d9d60c48c6e0d7500234b2168791ff04
Ganeshbjamkar <you@example.com>
2023-11-11 16:41:17 +0530

    ExpenseDate and  ExpenseDeatils

---
+++ src/components/ExpenseDetails.js
@@ -0,0 +1,10 @@
+const ExpenseDetails = (props) => {
+  return (
+    <div className="expense-details">
+      <h2>{props.title}</h2>
+      <div className="expense-amount">{props.amount}</div>
+    </div>
+  );
+};
+
+export default ExpenseDetails;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,14 @@
 // ExpenseItem.js
 import React from "react";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails title={props.title} amount={props.amount} />
       <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
