47385e8f2bb27022831c952e3397c39d108ef1e9
Ganesh Jamkar <ganesh.b.jamkar@gmail.com>
2023-12-07 13:28:40 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ const App = () => {
   const expenses = [
     {
       id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
+      title: "Clothes",
+      amount: 44.55,
+      date: new Date(2023, 9, 21),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2", title: "Mobile", amount: 300.48, date: new Date(2023, 10, 18) },
     {
       id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
+      title: "Bike Insurance",
+      amount: 298.9,
+      date: new Date(2023, 10, 28),
     },
     {
       id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
+      title: "Table",
+      amount: 150,
+      date: new Date(2023, 10, 12),
     },
   ];
 
--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,21 +1,15 @@
-// ExpenseItem.js
 import React from "react";
+
 import ExpenseDate from "./ExpenseDate";
-import ExpenseDetails from "./ExpenseDetails";
-import "./ExpenseItem.css";
 import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <ExpenseDetails title={props.title} amount={props.amount} />
-      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__locationofexpenditure">
-          {props.locationofexpenditure}
-        </div>
         <div className="expense-item__price">${props.amount}</div>
       </div>
     </Card>
