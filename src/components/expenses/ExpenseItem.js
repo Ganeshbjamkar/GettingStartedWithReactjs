359080ceb9d340406bb7055f6f7b1dade00035f2
Ganesh Jamkar <ganesh.b.jamkar@gmail.com>
2024-01-18 23:58:16 +0530

    PlaceHolder - Adding a filter which doesnt work on the expense tracker

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
@@ -6,35 +6,35 @@ import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   // function clickHandler() {}
-  const [title, setTitle] = useState(props.title);
-  console.log("ExpenseItem evaluated by React");
+  // const [title, setTitle] = useState(props.title);
+  // console.log("ExpenseItem evaluated by React");
 
-  const [amount, setAmount] = useState(props.amount);
+  // const [amount, setAmount] = useState(props.amount);
 
-  const clickHandler = () => {
-    setTitle("Updated!");
-    console.log(title);
-  };
+  // const clickHandler = () => {
+  //   setTitle("Updated!");
+  //   console.log(title);
+  // };
 
-  const changeAmountHandler = () => {
-    setAmount(100);
-    console.log(amount);
-  };
+  // const changeAmountHandler = () => {
+  //   setAmount(100);
+  //   console.log(amount);
+  // };
 
-  const deleteHandler = () => {
-    console.log("Deleted Item!!");
-  };
+  // const deleteHandler = () => {
+  //   console.log("Deleted Item!!");
+  // };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={clickHandler}>Change Title</button>
       <button onClick={changeAmountHandler}>Change Amount</button>
-      <button onClick={deleteHandler}>Delete Item</button>
+      <button onClick={deleteHandler}>Delete Item</button> */}
     </Card>
   );
 };
